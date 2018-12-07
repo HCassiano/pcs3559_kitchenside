22aa82aa70661e1fe15d971552399fad7a5c99f5
HCassiano <henrique.cassiano.barros@usp.br>
2018-12-07 20:38:27 -0200

    fisrt unreal integration test

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
@@ -10,3 +11,28 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool',(req,res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+*/
+
+var express = require('express');
+var app = express();
+const bodyParser = require("body-parser");
+
+// parse application/x-www-form-urlencoded and json
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json());
+
+app.post('/ingredients', function (req, res) {
+  //var ingredients = JSON.parse()
+  var ingredients = req.body.ingredients
+  var status = "{ " + "\"status\": " + "\"OK\" }"
+  console.log(ingredients)
+  res.send(status);
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
